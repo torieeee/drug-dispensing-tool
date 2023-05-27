c0f46966082de393a56210cbc95175e71cde642b
Lee <123798682+Calmcat1@users.noreply.github.com>
2023-05-27 20:47:51 +0300

    Add files via upload

--- drug dispensing tool/JS/formfunctionality.js
+++ drug dispensing tool/JS/formfunctionality.js
@@ -2,22 +2,50 @@ function toggleswitcher(elmnt,page){
 
   let toggle,togglecontent
 
-  togglecontent = document.getElementsByClassName("togglecontent")
+  togglecontent = document.getElementsByClassName("togglecontent");
   
   for(i=0; i<togglecontent.length; i++){
-    togglecontent[i].style.display = "none"
+    togglecontent[i].style.display = "none";
   }
 
 
 
 
-  toggle = document.getElementsByClassName("togglebtn")
+  toggle = document.getElementsByClassName("togglebtn");
 
   for(i=0; i<toggle.length; i++){
-    toggle[i].style.backgroundColor = ""
+    toggle[i].style.backgroundColor = "";
   }
 
-  elmnt.style.backgroundColor = "white"
-  document.getElementById(page).style.display = "flex"
+
+  elmnt.style.backgroundColor = "white";
+  document.getElementById(page).style.display = "flex";
 
 }
+
+
+function accountSwitcher(accountType){
+
+  let accountSelector;
+
+  accountSelector = document.getElementsByClassName("acctype");
+
+  for(i=0; i<accountSelector.length; i++){
+    accountSelector[i].style.display = "none";
+  }
+
+  document.getElementById(accountType).style.display = "block";
+}
+
+
+function accountCloser(){
+
+  accountSelector = document.getElementsByClassName("acctype");
+
+  for(i=0; i<accountSelector.length; i++){
+    accountSelector[i].style.display = "none";
+  }
+
+}
+
+document.getElementById("defaultOpen").click();
