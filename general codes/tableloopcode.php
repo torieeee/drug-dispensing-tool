fc918806a4416039b099c2bc3131cf800ccb1b9b
Lee <123798682+Calmcat1@users.noreply.github.com>
2023-07-12 14:31:08 +0300

    Add files via upload
    These are codes that are templates, basically providing functionality to php

---
+++ general codes/tableloopcode.php
@@ -0,0 +1,36 @@
+<?php
+
+// template code for table Looping (makes a table from the DB)
+
+
+include_once("Database_connection");
+
+
+              $sql = "SELECT fields FROM `tablename`";
+              $result = $conn->query($sql);
+              if ($result -> num_rows > 0){
+                echo "<table>";
+                echo "<tr><th>fieldVal1</th><th>fieldVal2</th></tr>"; 
+                while($row = $result->fetch_assoc()){       
+                  echo "<tr><td>".($row['fieldVal1'])."</td><td>".($row['fieldVal2'])."</td></tr>";
+                }
+              echo "</table>";
+            }
+                
+                else{
+                  echo "0 rows are present";
+                }
+
+
+/*
+
+keys -> field vals -> these are fields in your table
+
+-> database connection-> where your connections is
+
+Self explanatory
+-> fields
+-> tablename
+
+
+?>
