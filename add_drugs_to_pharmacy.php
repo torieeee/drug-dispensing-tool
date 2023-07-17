92352c264008a49c17ed79ee856ffbcd6b358daa
torieeee <131009366+torieeee@users.noreply.github.com>
2023-07-17 16:02:02 +0300

    Add files via upload

--- add_drugs_to_pharmacy.php
+++ add_drugs_to_pharmacy.php
@@ -13,7 +13,7 @@ $drugNo=$_POST['drug_no'];
 $quantity=$_POST['formula'];
 $company=$_POST['company'];
 $price=$_POST['price'];
-$sql="INSERT INTO drugs in pharmacy(drug_no,quantity_in_mgs,pharmacy_name,company,price)
+$sql="INSERT INTO drug in pharmacy(drug_no,quantity_in_mgs,pharmacy_name,company,price)
 VALUES('$drugNo','$quatity','$Name','$company','$price')";
 if($conn->query($sql)===TRUE){
     echo"New drug added succesfully";
